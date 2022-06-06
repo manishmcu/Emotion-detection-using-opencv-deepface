95b5a9e5c488c9e57af9e2435d3dac8890b2cbb6
Manish Pakhira <48309293+manishmcu@users.noreply.github.com>
2022-06-06 12:55:48 +0530

    Update Arduino_pySerial.ino

--- Arduino_pySerial/Arduino_pySerial.ino
+++ Arduino_pySerial/Arduino_pySerial.ino
@@ -1,9 +1,17 @@
+#include <Servo.h>
+
+Servo myservo;
+int pos = 0;
+int val = 0;
+
 char serialData;
 int pin=8;
 
 void setup() {
-pinMode(pin,OUTPUT);
+myservo.attach(9);
+//pinMode(pin,OUTPUT);
 Serial.begin(9600);
+myservo.write(0); 
 }
 
 void loop() {
@@ -12,10 +20,58 @@ if(Serial.available() > 0)
 serialData = Serial.read();
 Serial.print(serialData);}
 
-if(serialData == 'H')
-{digitalWrite(pin,HIGH);
-delay(100);}
+if(serialData == 'N' && val > 1)
+{
+val = 1;
+  for (pos = pos; pos >= 0; pos -= 1)
+  {  myservo.write(pos); delay(15); }
+  delay(1000);
+}
+
+if(serialData == 'H' && val < 2)
+{
+val = 2;
+  for (pos = pos; pos <= 45; pos += 1) 
+  { myservo.write(pos); delay(5);  }
+  delay(1000);
+}
+
+if(serialData == 'H' && val > 2)
+{
+val = 2;
+  for (pos = pos; pos >= 45; pos -= 1)
+  {  myservo.write(pos); delay(5); }
+  delay(1000);
+}
+
+if(serialData == 'D' && val < 3)
+{
+val = 3;
+  for (pos = pos; pos <= 90; pos += 1) 
+  { myservo.write(pos); delay(5);  }
+  delay(1000);
+}
+
+if(serialData == 'D' && val > 3)
+{
+val = 3;
+  for (pos = pos; pos >= 90; pos -= 1)
+  {  myservo.write(pos); delay(5); }
+  delay(1000);
+}
+
+if(serialData == 'A' && val < 4)
+{
+val = 4;
+  for (pos = pos; pos <= 135; pos += 1) 
+  { myservo.write(pos); delay(5);  }
+  delay(1000);
+}
+
 else
-{digitalWrite(pin,LOW);}
+{
+//    for (pos = pos; pos >= 0; pos -= 1)
+//  {  myservo.write(pos); delay(2); }
+}
 
 }
